ae731f9525353ab3426eae149d5193e193a64b19
Alina-kio <alinaolkim@gmail.com>
2023-08-23 01:07:11 +0600

    third commit

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,11 +6,12 @@ from .models import CustomUser
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'username', 'password']
+        fields = ['email', 'username', 'password', 'confirm_password']
+        extra_kwargs = {"confirm_password": {"write_only": True}}
 
     def validate(self, data):
-        # if data['password'] != data['confirm_password']:
-        #     raise serializers.ValidationError("Passwords do not match.")
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
             # return data
         
         if len(data) > 5:
